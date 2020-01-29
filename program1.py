d718cf876ae223ceac270058763e7c538246bb10
Andrew <43796708+andrewwolf0@users.noreply.github.com>
2020-01-29 17:45:45 -0600

    Update program1.py
    Created board along with debug
    
    Able to insert numbers into the board
    
    Added whiteboard comments

--- program1.py
+++ program1.py
@@ -3,7 +3,7 @@ from random import randint
 board = []
 
 for x in range(6):
-  board.append(["O"] * 7)
+  board.append([0] * 7) #7 are the collumns, 6 are the rows
 
 def print_board(board):
  r = 0
@@ -14,8 +14,47 @@ def print_board(board):
      print board[r][c]
      c = c + 1
   r=r+1
-print_board(board)
 
+print "The original board is: \n"
+
+for row in enumerate(board):
+    print row
+print('________________________________________\n')
+#assigning variable of target, first number is row (starting from top) then column next
+target = board[1][3]
+print("Target is \n")
+target = target + 1
+#Inserts a number into the board using the coordinates 1, 3
+board[1][3] = target
+print target
+
+print('________________________________________\n')
+
+print("Changed board is \n")
+for row in enumerate(board):
+    print row
+#insert_chip(col_num)
+#Highest # row we can drop it into
+#check if there is a 1 or 2
+#for loop to loop
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print board[0]
+#print_board(board)
 # def random_row(board):
 #   return randint(0, len(board) - 1)
 #
