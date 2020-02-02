cf3da27c3263826dbb207cb63f47ff36fbc2d094
K-Miles <43188266+K-Miles@users.noreply.github.com>
2020-02-02 12:02:11 -0600

    Update program1.py
    saturday class

--- program1.py
+++ program1.py
@@ -1,45 +1,52 @@
-from random import randint
-
-board = []
-
-for x in range(6):
-  board.append([0] * 7) #7 are the collumns, 6 are the rows
+##########################################################################
+# preperation to main code block
+##########################################################################
 
 def change_board(board, row, col):
- 	target = board[row][col]
- 	if target == 0:
- 		target = target + 1
- 	 	board[row][col] = target
- 	if target == 1:
- 		row = row - 1
- 		change_board(board, row, col)
-         if row < 0:
-             print"That column is full. Please choose another column"
-             print"\n"
-             col = int(raw_input("Type the column number you would like to place your chip in: "))
-             change_board(board, row, col)
+ #print row
+ #print col
 
-def player1(num):
-	num = col
-	if num < 7:
-		change_board(board, row, col)
-		target = target + 1
-	else:
-            print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
-        print "\n"
-        col = int(raw_input("Type the column number you would like to place your chip in: "))
-        player1(num)
+ currentRow = board[row]
+ #print currentRow
+ #print currentRow[col]
 
-def player2(num):
-    num = col
-    if num < 7:
-        change_board(board, row)
-        target = target - 2
-    else:
-        print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
-        print "\n"
-        col = int(raw_input("Type the column number you would like to place your chip in: "))
-        player2(num)
+
+ target = currentRow[col]
+ if target == 0:
+   #target = target + 1
+   board[row][col] = 1
+ if target == 1 or target == 2:
+   row = row - 1
+   change_board(board, row, col)
+ if row < 0:
+   print"That column is full. Please choose another column"
+   print"\n"
+   col = int(raw_input("Type the column number you would like to place your chip in: "))
+   change_board(board, row, col)
+
+def player1(board, row, col):
+  print row
+  print col
+  num = col
+  if num < 7:
+    change_board(board, row, col)
+    target = target + 1
+  else:
+    print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
+    print "\n"
+    col = int(raw_input("Type the column number you would like to place your chip in: "))
+    player1(num)
+
+def player2(board, row, col):
+  num = col
+  if num < 7:
+    change_board(board, row)
+    target = target - 2
+  else:
+    print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
+    print "\n"
+    col = int(raw_input("Type the column number you would like to place your chip in: "))
+    player2(num)
 
 def print_board(board):
  r = 0
@@ -51,31 +58,45 @@ def print_board(board):
      c = c + 1
   r=r+1
 
-print "The original board is: \n"
+##########################################################################
+# main program block
+##########################################################################
+board = []
 
-for row in enumerate(board):
-    print row
-print('________________________________________\n')
-#assigning variable of target, first number is row (starting from top) then column next
-target = board[1][3]
-print("Target is \n")
-target = target + 1
-#Inserts a number into the board using the coordinates 1, 3
-board[1][3] = target
-print target
+for x in range(6):
+  board.append([0] * 7) #7 are the columns, 6 are the rows
 
-print('________________________________________\n')
-
-print("Changed board is \n")
-for row in enumerate(board):
-    print row
+# print "The original board is: \n"
+#
+# for row in enumerate(board):
+#     print row
+#
+# print('________________________________________\n')
+#
+# #assigning variable of target, first number is row (starting from top) then column next
+# #target = board[1][3]
+# #print("Target is \n")
+# target = target + 1
+# #Inserts a number into the board using the coordinates 1, 3
+# board[1][3] = target
+# print target
+#
+# print('________________________________________\n')
+#
+# print("Changed board is \n")
+# for row in enumerate(board):
+#     print row
 #new code is under here
+
 num = 0
 row = 5
+target = 0
 
 #col = int(raw_input("Type the column number you would like to place your chip in: "))
 
-print board
+#print board
+for r in enumerate(board):
+    print r
 
 print"\n"
 
@@ -85,9 +106,10 @@ print "________________________\n"
 
 col = int(raw_input("Type the column number you would like to place your chip in: "))
 
-player1(num)
+player1(board,row,col)
 
-print board
+for r in enumerate(board):
+    print r
 
 print "\n"
 
@@ -97,7 +119,7 @@ print "________________________\n"
 
 col = int(raw_input("Type the column number you would like to place your chip in: "))
 
-player2(num)
+player2(board,row,col)
 #insert_chip(col_num)
 #Highest number row we can drop it into
 #check if there is a 1 or 2
