dfe77be131d46cd1a7377cc2aefa33fe7f3cf291
Andrew <43796708+andrewwolf0@users.noreply.github.com>
2020-02-09 12:25:32 -0600

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,64 +1,40 @@
 ##########################################################################
 # preperation to main code block
 ##########################################################################
-
-#def change_board(board, row, col):
- #if target == 0
- #currentRow[col] = 1
- #if target == 1 or target == 2:
-   #row = row - 1
-   #change_board(board, row, col)
 ##########################################################################
  ##if row < 0:
-   ##print"That column is full. Please choose another column"
-   ##print"\n"
+   ##print("That column is full. Please choose another column")
+   ##print("\n")
    ##col = int(raw_input("Type the column number you would like to place your chip in: "))
-   ##change_board(board, row, col)
+   ##change_board(board, col)
 ##########################################################################
 
-def change_board(board: list, col: int): #target = number (ex: 1 or 2)
+def change_board(board: list, col: int, player_chip: int): #target = number (ex: 1 or 2)
     row_default = 5
-    target = currentRow[col]
-    currentRow = board[row]
-    while target == 1 or target == 2:
-        row_default = row_default - 1
-        #target = currentRow[col]
-        if row < 0 or target == 0:
+    chipin = False
+    while row_default >= 0:
+        currentRow = board[row_default]
+        if currentRow[col] == 0:
+            currentRow[col] = player_chip
+            chipin = True
             break
-    if target == 0:
-        target = target + 1
-        currentRow[col] = target
-    return board #updated board
+        else:
+            row_default = row_default - 1
+    return chipin #returns if the chip was placed
+
+def print_board(board):
+    for r in enumerate(board):
+        print (r)
 
 def player1(board, col):
   #turn = turn + 1
-  if col < 7:
-    change_board(board, col)
-  else:
-    print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
-    print "\n"
-    col = int(raw_input("Type the column number you would like to place your chip in: "))
-    player1(board, col)
+  result = change_board(board, col, 1)
+  print ("Player one " + str(result))
 
 def player2(board, col):
   #turn = turn + 1
-  if col < 7:
-    change_board(board, col)
-  else:
-    print "The column number must be 0, 1, 2, 3, 4, 5, or 6. Please try again"
-    print "\n"
-    col = int(raw_input("Type the column number you would like to place your chip in: "))
-    player2(board, col)
-
-# def print_board(board):
-#  r = 0
-#  for row in board:
-#   c = 0
-#   for cell in row:
-#      print str(r) + " " + str(c)
-#      print board[r][c]
-#      c = c + 1
-#   r=r+1
+    result = change_board(board, col, 2)
+    print ("Player two " + str(result))
 
 ##########################################################################
 # main program block
@@ -73,45 +49,51 @@ for x in range(6):
 num = 0
 target = 0
 
-#col = int(raw_input("Type the column number you would like to place your chip in: "))
+print_board(board)
 
-#print board
-for r in enumerate(board):
-    print r
+while 1 == 1:
+    print("\n")
 
-print"\n"
+    print ("Player one's turn")
 
-print"Player one's turn"
+    print ("________________________\n")
 
-print "________________________\n"
+    col = 0
+    while 1==1: #to make better next session
+        col = int(input("Type the column number you would like to place your chip in: "))
+        if col >= 0 and col <= 6:
+            break
+        else:
+            print ("Please enter a number between 0 and 6")
 
-col = int(raw_input("Type the column number you would like to place your chip in: "))
+    player1(board, col)
 
-player1(board, col)
+    print_board(board)
 
-for r in enumerate(board):
-    print r
+    print ("\n")
 
-print "\n"
+    print ("Player two's turn")
 
-print "Player two's turn"
+    print ("________________________\n")
 
-print "________________________\n"
+    while 1==1: #to make better next session
+        col = int(input("Type the column number you would like to place your chip in: "))
+        if col >= 0 and col <= 6:
+            print ("\n")
+            break
+        else:
+            print ("Please enter a number between 0 and 6")
 
-col = int(raw_input("Type the column number you would like to place your chip in: "))
+    player2(board, col)
 
-player2(board, col)
-#insert_chip(col_num)
-#Highest number row we can drop it into
-#check if there is a 1 or 2
-#for loop to go through list
-#only write in the selected column
+    print_board(board)
 
 ##########################################################################
 # change_board function with turn functionality
 ##########################################################################
 
 # def change_board(board: list, col: int): #target = number (ex: 1 or 2)
+#     turn = 0
 #     row_default = 5
 #     target = currentRow[col]
 #     currentRow = board[row]
@@ -123,8 +105,10 @@ player2(board, col)
 #     if target == 0:
 #         if turn == 0:
 #             target = target + 1
+#             turn = turn + 1
 #         if turn !% 2:
 #         target = target + 2
+#         turn = turn + 1
 #         currentRow[col] = target
 #         else:
 #             target = target + 1
