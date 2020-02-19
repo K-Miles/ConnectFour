086833999b608e91057ee3e02d1d4c881a4feece
Andrew <43796708+andrewwolf0@users.noreply.github.com>
2020-02-18 19:10:38 -0600

    Cleanup + Working to combine P1 and P2 functions
    Cleaned up old functions that were commented out
    
    Started work to combine the Player1 and Player2 functions

--- connectfour.py
+++ connectfour.py
@@ -1,13 +1,6 @@
 ##########################################################################
 # preperation to main code block
 ##########################################################################
-##########################################################################
- ##if row < 0:
-   ##print("That column is full. Please choose another column")
-   ##print("\n")
-   ##col = int(raw_input("Type the column number you would like to place your chip in: "))
-   ##change_board(board, col)
-##########################################################################
 
 def change_board(board: list, col: int, player_chip: int): #target = number (ex: 1 or 2)
     row_default = 5
@@ -26,13 +19,11 @@ def print_board(board):
     for r in enumerate(board):
         print (r)
 
-def player1(board, col):
-  #turn = turn + 1
-  chipin = change_board(board, col, 1)
+def player1(board, col, player = 1: int):
+  chipin = change_board(board, col, player)
   return chipin
 
 def player2(board, col):
-  #turn = turn + 1
     chipin = change_board(board, col, 2)
     return chipin
 
@@ -49,8 +40,6 @@ board = []
 
 for x in range(6):
   board.append([0] * 7) #7 are the columns, 6 are the rows
-
-#new code is under here
 
 num = 0
 target = 0
@@ -107,30 +96,3 @@ while 1 == 1:
     print ("Player Two: " + str(chipin))
 
     print_board(board)
-
-##########################################################################
-# change_board function with turn functionality
-##########################################################################
-
-# def change_board(board: list, col: int): #target = number (ex: 1 or 2)
-#     turn = 0
-#     row_default = 5
-#     target = currentRow[col]
-#     currentRow = board[row]
-#     while target == 1 or target == 2:
-#         row_default = row_default - 1
-#         #target = currentRow[col]
-#         if row < 0 or target == 0:
-#             break
-#     if target == 0:
-#         if turn == 0:
-#             target = target + 1
-#             turn = turn + 1
-#         if turn !% 2:
-#         target = target + 2
-#         turn = turn + 1
-#         currentRow[col] = target
-#         else:
-#             target = target + 1
-#             currentRow[col] = target
-#     return board #updated board
