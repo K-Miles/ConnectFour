35b27d00e58c6742275e64e8f904115af38668e8
Andrew <43796708+andrewwolf0@users.noreply.github.com>
2020-02-22 14:30:39 -0600

    Working on FourInARow(Column)
    Is detecting Four in a Row, but only above Column 5 and giving a "local variable referenced before assisgnment" error

--- connectfour.py
+++ connectfour.py
@@ -1,6 +1,8 @@
 ##########################################################################
 # preperation to main code block
 ##########################################################################
+player1_score = 0
+player2_score = 0
 
 def change_board(board: list, col: int, player_chip: int): #target = number (ex: 1 or 2)
     row_default = 5
@@ -13,13 +15,18 @@ def change_board(board: list, col: int, player_chip: int): #target = number (ex:
             break
         else:
             row_default = row_default - 1
+    if chipin == True:
+        FourInARowCol(board)
     return chipin #returns if the chip was placed
 
 def print_board(board):
     for r in enumerate(board):
         print (r)
+    print ("\n")
+    print ("Player One Score = " + str(player1_score))
+    print ("Player Two Score = " + str(player2_score))
 
-def player1(board, col, player = 1: int):
+def player1(board, col, player = 1):
   chipin = change_board(board, col, player)
   return chipin
 
@@ -28,6 +35,37 @@ def player2(board, col):
     return chipin
 
 def IsBoardFull(board):
+
+    return False
+
+def FourInARowCol(board):
+    print("hi")
+    for row in range(len(board) - 4):
+        for col in range(len(board)):
+            if (board[row][col] == board[row + 1][col]) and (board[row + 2][col] == board[row+3][col]) and (board[row][col] == board[row + 2][col]):
+                if board[row][col] == 1:
+                    player1_score += 1
+                elif board[row][col] == 2:
+                    player2_score += 1
+                else:
+                    pass
+
+
+def FourInARowRow(board):
+    #for row in len(board):
+        #for col in len(board) - 4:
+            #if board[row][col] == board[row][col + 1] and board[row][col + 2] == board[row][col + 3] and board[row][col] == board[row][col + 2]:
+                #if board[row][col] == 1:
+                    #player1_score += 1
+                #elif board[row][col] == 2:
+                    #player2_score += 1
+                #else:
+                    #pass
+
+    return False
+
+def FourInARowDiagonal(board):
+
     return False
 
 #use loop to check if each column is full
@@ -43,6 +81,8 @@ for x in range(6):
 
 num = 0
 target = 0
+# player1_score = 0
+# player2_score = 0
 
 print_board(board)
 
@@ -80,9 +120,9 @@ while 1 == 1:
     print ("________________________\n")
 
     chipin = False
-    while chipin == False:
+    while not chipin:
         col = -1
-        while col < 0 and col > 6:
+        while col < 0 or col > 6:
             col = int(input("Player Two: Type the column number you would like to place your chip in: "))
             if col < 0 or col > 6:
                 print ("\n")
